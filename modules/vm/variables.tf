7b7f0b2dfdbdd7e132c9b6c76e87af38b4de2996
Shashanksingh15101998 <shashanksingh179.ss@gmail.com>
2026-01-08 12:14:36 +0530

    Create variables.tf

---
+++ modules/vm/variables.tf
@@ -0,0 +1,15 @@
+variable "vm_name" {
+    description = " vm name"
+}
+ 
+variable "zone" {
+    description = "vm zone"
+}
+ 
+variable "network_id" {
+    description = "vpc network id"
+}
+ 
+variable "subnet_id" {
+    description = "Subnet id"
+}
